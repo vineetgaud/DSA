fcc47789aefa79aea50fe48ce3bdc7d09c5de920
vineetgaud <vineettgaud9696@gmail.com>
2025-10-05 20:26:55 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,40 @@
+// public class oops {
+//        public static void main(String[] args) {
+//         pen pi=new pen();
+//         pi.setcolor("green");
+//         pi.settip(5);
+//         System.out.println(pi.getcolor());
+//         System.out.println(pi.gettip());
+//         bankaccount v=new bankaccount();
+//         v.username = "vinit";
+//         System.out.println(v.username);
+//         v.setpassword("aabb");
+        
+//        }    
+// }
+//  class pen {       
+//  private String color;
+//  private int tip;
+// String getcolor(){
+//     return this.color;
+// }
+// int gettip(){
+//     return this.tip;
+// }
+// void setcolor(String newcolor){
+//     color=newcolor;
+// }
+// void settip(int newtip){
+//     tip= newtip;
+// }
+    
+// }
+// // acess modifiers
+// class bankaccount{
+//     public String username;
+//     private String password;
+//     public void setpassword(String pwd){
+//      password=pwd;
+//     }
+
+// }
