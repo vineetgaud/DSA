654c75180ff90fc7e6adca8f8a1982c65005d0bf
vineetgaud <vineettgaud9696@gmail.com>
2025-10-06 19:31:33 +0530

    Add files via upload

---
+++ OopsInheritance.java
@@ -0,0 +1,47 @@
+// public class OopsInheritance {
+//     public static void main(String[] args) {
+//        // fish shark=new fish();
+       // shark.eat();
+    //    dog dobby = new dog();
+    //    dobby.eat();
+    //    dobby.legs=4;
+    //    System.out.println(dobby.legs);
+    //    calculate cal=new calculate();
+    //    System.out.println(cal.sum(3, 5));
+    //    System.out.println(cal.sum((float)1.4, (float)1.5));
+    //    System.out.println(cal.sum(4, 4, 7));
+    //  }
+//}
+// class Animal{
+//     String color;
+//     void eat(){
+//         System.out.println("eats");
+//     }
+//     void breathe(){
+//         System.out.println("breathes");
+//     }   
+// }
+// class Mammals extends Animal{
+//    int breed;
+// }
+// class dog extends Mammals{
+//     int legs;
+//}
+// class fish extends Animal{
+// int fins;
+// void swims(){
+//     System.out.println("swims in water");
+// }
+//}
+// class calculate{
+//     int sum(int a,int b){
+//       return  a+b;
+//     }
+//     float sum(float a, float b){
+//         return a + b;
+//     }
+//     int sum(int a, int b, int c){
+//         return a+b+c;
+//     }
+// }
+    
