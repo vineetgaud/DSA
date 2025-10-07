4b53a6333235d2a6c3762727129ce9167791c3c1
vineetgaud <vineettgaud9696@gmail.com>
2025-10-07 21:03:00 +0530

    Add files via upload

---
+++ OopsconStructor.java
@@ -0,0 +1,60 @@
+// public class OopsconStructor {
+//     public static void main(String[] args) {
+        //Student a= new Student();
+        //Student v=new Student("vinit");
+       // System.out.println(v.name);
+       // constructor
+//       employes e1= new employes();
+//       e1.employee="vinit";
+//       e1.salary =200000;
+//       e1.password="xyz";
+//       e1.marks[0]=50;
+//       e1.marks[1]=70;
+//       e1.marks[2]=90;
+//       // copy constructor
+//       employes e2= new employes(e1);
+//       e1.marks[2]=100;
+//       for (int i = 0; i <3; i++) {
+//         System.out.println(e2.marks[i]);
+//       }
+      
+//     }
+// }
+// class Student {
+//     String name;
+//     int age;
+//     Student(){
+//       System.out.println("constructor is called....");
+//     }
+//     Student(String name){
+//         this.name=name;
+//     }
+//}
+
+// copy constructor
+// class employes {
+//     String employee;
+//     int salary;
+//     String password;
+//     int marks[];
+//     // copy constructor
+//     employes(employes e1){
+//         marks=new int[3];
+//         this.employee=e1.employee;
+//         this.salary=e1.salary;
+//         this.password=e1.password;
+//         this.marks=e1.marks;
+//     }
+//     employes(){
+//        marks=new int[3];
+//       System.out.println("constructor is called....");
+//     }
+//     employes(String employee){
+//         marks=new int[3];
+//         this.employee=employee;
+//     }
+//     employes(int salary){
+//         marks=new int[3];
+//         this.salary=salary;
+//     }
+// }
