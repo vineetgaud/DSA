e56046e5aae99fe52ec6980ab491de7943f72934
vineetgaud <vineettgaud9696@gmail.com>
2025-10-09 22:38:02 +0530

    Add files via upload

---
+++ OopsAbstract.java
@@ -0,0 +1,63 @@
+// public class OopsAbstract {
+//     public static void main(String[] args) {
+    //     Horse aman= new Horse();
+    //     aman.eat();
+    //     aman.walk();
+    //     aman.changecolor();
+    //    Hen h= new Hen();
+    //    // animal->horse->hen
+    //    student s1=new student();
+    //    s1.clgname="dpdc";
+    //    student s2=new student();
+    //    System.out.println(s2.clgname);
+    //     student s3=new student();
+    //    System.out.println(s3.clgname);
+    // }
+//}
+// abstract class Animal {
+//     String color;
+//     Animal(){
+//         color="brown";
+//         System.out.println("animal constructor");
+//     }    
+// void eat(){
+//     System.out.println("animals eats");
+// }
+//     abstract void walk();
+// }
+// class Horse extends Animal{
+//     Horse(){
+//          System.out.println("horse constructor");
+//     }
+//      void changecolor(){
+//         color="dark brown";
+//      }
+//     void walk(){
+//         System.out.println("animal walks in 4 legs");
+//     }
+// }
+// class Hen extends Animal{
+//     Hen(){
+//          System.out.println("hen constructor");
+//     }
+//     void changecolor(){
+//         color="yello";
+//      }
+//     void walk(){
+//          System.out.println("animal walks in 2 legs");
+//     }
+// }
+
+
+// // static keyword
+// class student{
+//     int a=20;
+//     String name ;
+//     static String clgname;
+//     void setname(String name){
+//         this.name=name;
+//     }
+//     String getname(){
+//         return this.name;
+//     }
+// }
