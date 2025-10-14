fd0ede39d3ddd99e2306683ae1909e81942cbfa3
vineetgaud <vineettgaud9696@gmail.com>
2025-10-14 21:54:48 +0530

    Add files via upload

---
+++ inputjava.java
@@ -0,0 +1,17 @@
+
+// import java.util.Scanner;
+// public class inputjava{
+// public static void main(String[] args) {
+//      Scanner sc = new Scanner(System.in);
+//         int marks[] = new int[100];
+// System.out.println("length array="+marks.length);
+//         marks [0]=sc.nextInt();
+//         marks[1]=sc.nextInt();
+//         marks[2]=sc.nextInt();
+//         System.out.println("phy:" +marks[0]); 
+//         System.out.println("che:" +marks[1]); 
+//         System.out.println("math:" +marks[2]); 
+//         int percentage=(marks[0]+marks[1]+marks[2])/3;
+//         System.out.println("percentage ="+percentage+"%");
+//     }
+//  }
