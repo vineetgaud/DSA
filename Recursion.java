ec69ef91243017d7355134769989747a4a638912
vineetgaud <vineettgaud9696@gmail.com>
2025-10-15 20:50:41 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,87 @@
+//public class Recursion {
+   /* 
+     public static void printdec(int n){
+        if (n==1) {
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printdec(n-1);
+        
+    }
+    public static void main(String[] args) {
+        int n=10;
+       printdec(n);
+    }
+       */
+
+    // increase
+
+     /* 
+     public static void printinc(int n){
+        if (n==1) {
+            System.out.print(n+" ");
+            return;
+        }
+        printinc(n-1);
+          System.out.print(n+" ");
+        
+    }
+    public static void main(String[] args) {
+        int n=5;
+       printinc(n);
+    }
+*/
+      // factorial
+ /* 
+  public static int fact(int n){
+    if (n==0) {
+        return 1;
+    }
+   int fnm1=fact(n-1);
+   int sn=n*fnm1;
+   return sn;
+}
+public static void main(String[] args) {
+    int n =5;
+  System.out.println(fact(n));
+}
+   */  
+
+    // sum of natural number
+    /* 
+public static int sum(int n){
+    if (n==1) {
+        return 1;
+    }
+   int snm1=sum(n-1);
+   int sn=n+snm1;
+   return sn;
+}
+public static void main(String[] args) {
+    int n =5;
+  System.out.println(sum(n));
+}
+  */
+
+    // Nth fibonacci number
+
+    /* 
+  public static int fibonacci(int n){
+    if (n==0 || n==1) {
+        return n;
+    }
+   int fnm1=fibonacci(n-1);
+   int fnm2=fibonacci(n-2);
+   int fn=fnm1+fnm2;
+   return fn;
+}
+public static void main(String[] args) {
+    int n =24;
+  System.out.println(fibonacci(23));
+  System.out.println(fibonacci(24));
+  System.out.println(fibonacci(25));
+}
+  */
+  
+//} 
